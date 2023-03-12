eae2db5153993a98cfa27d4fca72086070a321cc
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-12 23:56:40 +0100

    Detect sql injection
    Detect sql injection

---
+++ detectar_sqli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import mechanize
+from bs4 import BeautifulSoup
+
+nav = mechanize.Browser()
+nav.set_handle_robots(False)
+nav.set_handle_equiv(False)
+nav.addheaders = [('User-Agent','Firefox')]
+nav.open("http://192.168.0.21:42001/index.php")
+nav.select_form(nr=0)
+
+nav['username'] = 'admin'
+nav['password'] = 'password'
+
+nav.submit()
+
+nav.open("http://192.168.0.21:42001/vulnerabilities/sqli/")
+nav.select_form(nr=0)
+
+nav['id'] = "'"
+
+nav.submit()
+soup = BeautifulSoup(nav.response().read(),'html5lib')
+print(soup.pre.string)
