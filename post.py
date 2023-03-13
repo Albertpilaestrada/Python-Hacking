a179b10cc29d1133390779865281c01efc3cfa40
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-13 13:26:28 +0100

    Interact whit web sites
    Code to upload a file to a website using request.post()

---
+++ post.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+import argparse
+from os import path
+
+parser = argparse.ArgumentParser(description="detector de cabezeras")
+parser.add_argument('-f','--file',help="indica el achivo a subir")
+parser = parser.parse_args()
+
+def main():
+    if parser.file:
+        if path.exists(parser.file):
+            archivo=open(parser.file,'rb')
+            headers={'User-Agent':'Firefox'}
+            peticion=requests.post(url="http://direccion_de_la_pag_donde_vamos_a_subir_el_archivo",files={'Uploaded_file':archivo},headers=headers)
+            if parser.file in peticion.text:
+                print(peticion.text)
+                print("Archivo subido con exito")
+            else:
+                print("Fallo la subida del archivo")
+
+        else:
+            print("no exite el archivo")    
+    else:
+        print("necesito un archivo para subir")
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Saliendo")
+        exit()
