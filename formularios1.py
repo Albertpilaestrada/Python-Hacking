f8933f323abb9d4d42a50b1c5b68aad4ee1ad42d
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-13 13:46:54 +0100

    Interact whit web sites
    Code to interact with forms using mechanize

---
+++ formularios1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import mechanize
+import argparse
+from bs4 import BeautifulSoup
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-b",'--buscar',help="opcion a buscar")
+parser=parser.parse_args()
+
+
+def main():
+    if  parser.buscar:
+        bus=mechanize.Browser()
+        bus.set_handle_robots(False)
+        bus.set_handle_equiv(False)
+        bus.addheaders=[('User-Agent','Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; de) Opera 8.0')]
+        bus.open("https://www.google.com")
+        for n in bus.forms():
+            print(n)
+        bus.select_form(nr=0)
+        bus['q']=parser.buscar
+        bus.submit()
+        #print(bus.response().read())
+        p=BeautifulSoup(bus.response().read(),'html5lib')
+        for link in p.find_all('a'):
+            u=(link.get('href'))
+            u=u.replace('/url?q=','')
+            print(u)
+            
+
+        #for n in bus.forms():
+            #print(n)
+    else:
+        print("Palara a buscar")
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Saliendo")
