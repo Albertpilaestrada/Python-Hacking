8f0787df3e986291227939e54dbd27d02ab8ef2c
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-13 21:08:14 +0100

    Web Scanner
    This code scans a website and returns the wordpress version if used. The code of the web used is in the HTML5 repository

---
+++ version_worldpress.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url="http://localhost/HTML5/"
+    cabecera={'User-Agent':'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; de) Opera 8.0'}
+    peticion=requests.get(url=url,headers=cabecera)
+    #print(peticion.text)
+    soup=BeautifulSoup(peticion.text,'html5lib')
+    for v in soup.find_all('meta'):
+        if v.get('name')==('generator'):
+            version=v.get('content')
+    print(version)
+
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Saliendo")
+        exit()
