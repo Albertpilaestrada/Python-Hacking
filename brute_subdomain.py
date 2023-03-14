9923f7089c3742d736b8b132454206b8ddcf9af4
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-14 10:46:59 +0100

    Interacting with web logs
    With this code you can obtain the subdomains of a website through a brute force attack using a local dictionary

---
+++ brute_subdomain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import dns.resolver
+from os import path
+
+def main():
+    if path.exists('subdominios.txt'):
+        wordlist=open('subdominios.txt','r')
+        wordlist=wordlist.read().split('\n')
+        lista=[]
+        for s in wordlist:
+            try:
+                a=dns.resolver.resolve('{}.target_web'.format(s),'A')
+                lista.append('{},target_web'.format(s))
+            except:
+                pass
+        if len(lista)>0:
+            print("numero de subdominios posibles {}".format(len(lista)))
+            for e in lista:
+                print(e)
+        else:
+            print("no se encontraron subdominios")
+    else:
+         print("no existe el archivo")
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
