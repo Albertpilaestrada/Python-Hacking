c3cd9f0345e87cd2cd1cf4bca7cf6de02150d5a2
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-14 13:02:41 +0100

    Interacting with web logs
    This code is used to know if a website is protected by cloudflare

---
+++ cloudfare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+
+def main():
+    word="cloudflare"
+    url=requests.get("target_website")
+    cabeceras=dict(url.headers)
+    verify=False
+    for c in cabeceras:
+        if word in cabeceras[c].lower():
+            verify=True
+            break
+
+    if verify==True:
+        print("Cloudflare presente")
+    else:
+        print("No tiene cloudflare")
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
