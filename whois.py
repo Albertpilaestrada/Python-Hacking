74a952f694910c6a9c91938794cf32f1e78c507d
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-14 12:44:29 +0100

    Interacting with web logs
    With this code you can obtain the registration data of a domain using the whois service

---
+++ whois.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    a=requests.get("https://who.is/whois/traget_domain")
+    soup=BeautifulSoup(a.text,'html5lib')
+    for l in soup.find_all("pre"):
+        print(l.get_text())
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
