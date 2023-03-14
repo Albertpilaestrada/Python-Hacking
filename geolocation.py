2708ad5b3909651f0aa4266b891b6d8a8ebdd176
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-14 12:58:47 +0100

    Interacting with web logs
    This code is used to obtain the location of an IP using the ipinfo.io service

---
+++ geolocation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import json
+import urllib
+from urllib.request import urlopen
+
+def main():
+    url="https://ipinfo.io/target_ip/json"
+    v=urlopen(url)
+    j=json.loads(v.read())
+
+    for dato in j:
+        print(dato+" : "+ j[dato])
+
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
