1d3c92549e138889d7cb6a5a406a547b38d23f60
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-15 11:35:58 +0100

    Interact with OS
    Code to extract information from Windows OS

---
+++ info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+from subprocess import check_output
+import subprocess
+
+a=check_output('systeminfo',stderr=subprocess.STDOUT,encoding='cp1252')
+
+n=open('informaciondelsistema.txt','w+')
+n.write(a)
+print("Hola")
+n.close()
