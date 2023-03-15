950244cf42da118f9c1eb30d30439b298c72ced1
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-15 10:57:00 +0100

    Interact whit OS
    Code example of using the os module

---
+++ os.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import os
+
+print("Ruta actual: " + os.getcwd())
+#os.chdir("C:\Users\Albert\Desktop\Python")
+#print("Ruta actual: " + os.getcwd())
+print(os.listdir(os.getcwd()))
+#os.mkdir("prueba")
+#print(os.listdir(os.getcwd()))
+#os.rmdir("prueba")
+#print(os.listdir(os.getcwd()))
+#os.rename("prueba.txt","test.txt")
+#print(os.stat("test.txt"))
+os.system("ping www.google.com")
