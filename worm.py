0c4365711da7ed8896b8bdb55ddff9ed6b0e3feb
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-15 11:40:38 +0100

    Interact with OS
    tihs is a simple worm

---
+++ worm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import sys
+import shutil
+
+def main():
+    if len(sys.argv)==2:
+        for n in range(0,int(sys.argv[1])):
+            shutil.copy(sys.argv[0],sys.argv[0]+str(n)+'.py')
+    else:
+        print("Argumentos necesarios")
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
